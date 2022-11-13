0ea772a657551ffc41977ce22d5479e9b03395bb
blanray <104242797+blanray@users.noreply.github.com>
2022-11-13 13:01:57 -0300

    Inicio del proyecto
    Primer commit

---
+++ CrudPB/src/main/java/com/example/CrudPB/CrudPbApplication.java
@@ -0,0 +1,13 @@
+package com.example.CrudPB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudPbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudPbApplication.class, args);
+	}
+
+}
