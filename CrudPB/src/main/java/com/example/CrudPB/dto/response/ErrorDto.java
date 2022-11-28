16d88cfacff4616bfb5df58d064c4258fc2a16a2
blanray <blanray_winner@hotmail.com>
2022-11-28 08:29:25 -0300

    Excepcion personalizada de validacion

---
+++ CrudPB/src/main/java/com/example/CrudPB/dto/response/ErrorDto.java
@@ -0,0 +1,11 @@
+package com.example.CrudPB.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorDto {
+    private int statusCode;
+    private String message;
+}
